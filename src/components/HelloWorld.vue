7cb5374752bcec0f9accfe404364283d0407bc93
Simon McEnlly <simon.mcenlly@gmail.com>
2023-07-18 14:05:15 +1000

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <el-input v-model="input" @input="handlerInput"></el-input>
+    <el-input v-model="input" @input.native="handlerInput"></el-input>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
